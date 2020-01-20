cedd5dade2f7ad711bde5d1e43ac4ab9103abd4b
gaeljaffre <gael.jaffre@yahoo.fr>
2020-01-20 09:13:25 +0100

    Bandeau du haut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ListeContratsComponent } from './liste-contrats/liste-contrats.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ListeContratsComponent },
+      ]) ],
+  declarations: [ AppComponent, TopBarComponent, ListeContratsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
