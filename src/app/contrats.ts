662e44703fe95a433d4b95bbaa410cfdf9e2c181
gaeljaffre <gael.jaffre@yahoo.fr>
2020-01-20 10:19:02 +0100

    4 files modified, 2 added

---
+++ src/app/contrats.ts
@@ -0,0 +1,46 @@
+export const listeContrats = [
+  {
+    id: 1,
+    name: 'TOTAL',
+    description: 'The most famous client',
+    country: 'XX',
+    gin: '180012345678',
+    ddv: '01/01/2020',
+    dfv: '31/12/2020',
+  },
+  {
+    id: 2,
+    name: 'BUTAGAZ',
+    description: 'The best case study for Catherine',
+    country: 'FR',
+    gin: '180000000001',
+    ddv: '01/01/2020',
+    dfv: '31/12/2020',
+  },
+  {
+    id: 3,
+    name: 'BUTAGAZ',
+    description: 'The best example for IT',
+    country: 'FR',
+    gin: '180000000001',
+    ddv: '01/01/2021',
+    dfv: '31/12/2021',
+  },
+  {
+    id: 4,
+    name: 'BARILLA',
+    country: 'FR',
+    gin: '180000000002',
+    ddv: '01/01/2020',
+    dfv: '31/12/2020',
+  },
+  {
+    id: 5,
+    name: 'SUPERWURST',
+    country: 'DE',
+    gin: '180000000003',
+    ddv: '01/01/2020',
+    dfv: '31/12/2020',
+  },
+
+];
--- src/app/liste-contrats/liste-contrats.component.ts
+++ src/app/liste-contrats/liste-contrats.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { listeContrats } from '../contrats';
+
 @Component({
   selector: 'app-liste-contrats',
   templateUrl: './liste-contrats.component.html',
   styleUrls: ['./liste-contrats.component.css']
 })
 export class ListeContratsComponent implements OnInit {
+
+  contractsList = listeContrats;
 
   constructor() { }
 
